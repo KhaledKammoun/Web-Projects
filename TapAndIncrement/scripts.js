5ba573dd291a30925bd39f69a5f7c9dc80f5bd30
KhaledKammoun <khaledsfax03@gmail.com>
2023-12-12 19:10:10 +0100

    TapAndIncrement

---
+++ TapAndIncrement/scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var nbTotal = 0, nbCount = 0 ;
+    document.querySelector(".button").addEventListener("click", function(){
+        document.querySelector("#result-1").innerHTML = (++nbCount) ;
+        if ((nbCount) % 20 == 0){
+            document.querySelector("#result-2").innerHTML = (++nbTotal) ;
+            document.querySelector("#result-1").innerHTML = 0 ;
+        }
+    }) ;
+}) ;

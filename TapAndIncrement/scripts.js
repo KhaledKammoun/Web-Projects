e2021cd8573641ec0202c34bb673097c1c6833e3
KhaledKammoun <khaledsfax03@gmail.com>
2023-12-19 00:26:03 +0100

    TapAndIncrement

--- TapAndIncrement/scripts.js
+++ TapAndIncrement/scripts.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
     var tab_salat = [0, 0, 0, 0, 0];
-    var  nbCount = 0 ;
     var trElements = document.querySelectorAll("#table-1 tr");
-
     for (var i = 0; i < 5; i++) {
-        tab_salat[i] = trElements[i].querySelector('#val');
+        tab_salat[i] = trElements[i].querySelector('.val'+ i.toString());
     }
+
+    nbCount = 0 ;
     document.querySelector(".button").addEventListener("click", function(){
         document.querySelector("#result-1").innerHTML = (++nbCount) ;
         if ((nbCount) % 20 == 0){
@@ -20,5 +20,23 @@ document.addEventListener("DOMContentLoaded", function(){
             tab_salat[i].innerHTML++;
         }
     })
+
+    function changeValues(variable){
+        for (var i = 0; i < 5; i++) {
+            (function(index) {
+                document.querySelector('.button'+variable+'Val' + index.toString()).addEventListener("click", function() {
+                    if (variable == "Plus")
+                        tab_salat[index].innerHTML++;
+                    else if (variable == "Minus" && tab_salat[index].innerHTML >0)
+                        tab_salat[index].innerHTML-- ;
+                });
+    
+            })(i);
+        }
+    }
+    changeValues("Minus") ;
+    changeValues("Plus") ;
+
+
 }) ;
 
